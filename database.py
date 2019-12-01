e58151dccce6b55d0b38f5c0d9a49098c4fedf43
StoplightLoosejaw <57860542+StoplightLoosejaw@users.noreply.github.com>
2019-12-01 22:13:27 +0300

    Add files via upload

--- database.py
+++ database.py
@@ -1,15 +1,22 @@
-import sqlite3
 import random
+import re
+import os
+import psycopg2
 
-text_file = open("OZHEGOV.txt", "r")
+
+text_file = open("C:/Users/Tom/Desktop/OZHEGOV.txt", "r")
 lines = text_file.readlines()
 text_file.close()
+text_file_eng = open("C:/Users/Tom/PycharmProjects/telegabot/dictionary.csv", "r")
+lines_eng = text_file_eng.readlines()
+text_file_eng.close()
 
 
 class GallowsEngine:
-    def __init__(self, db_name="gallows_db.sqlite"):
-        self.db_name = db_name
-        self.conn = sqlite3.connect(db_name, check_same_thread=False)
+    def __init__(self):
+        DATABASE_URL = os.environ['DATABASE_URL']
+        self.conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+        self.cursor = self.conn.cursor()
 
     def setup(self):
         stmt = "CREATE TABLE IF NOT EXISTS CURRENT_GAME ( USER_ID text," \
@@ -18,49 +25,54 @@ class GallowsEngine:
                                                         " DESCRIPTION text," \
                                                         " CHARS text, MISTAKES integer)"
         stmt_users = "CREATE TABLE IF NOT EXISTS ALL_USERS ( USER_ID text," \
-                     " WINS integer, LOSSES integer,IMAGES_FLG integer )"
-        self.conn.execute(stmt)
-        self.conn.execute(stmt_users)
+                     " WINS integer, LOSSES integer,IMAGES_FLG integer, LANG_FLG integer )"
+
+        self.cursor.execute(stmt)
+        self.cursor.execute(stmt_users)
         self.conn.commit()
 
     def add_row_current_game(self, arcg_user_id, arcg_gallows_word,
                              arcg_gallows_status_word, arcg_description, arcg_chars, arcg_mistakes):
         stmt = "INSERT INTO CURRENT_GAME (USER_ID, GALLOWS_WORD," \
-               "GALLOWS_STATUS_WORD,DESCRIPTION,CHARS, MISTAKES) VALUES (?, ?, ?, ?, ?, ?)"
-        args = (arcg_user_id, arcg_gallows_word,
-                arcg_gallows_status_word, arcg_description, arcg_chars, arcg_mistakes, )
-        self.conn.execute(stmt, args)
+               "GALLOWS_STATUS_WORD,DESCRIPTION,CHARS, MISTAKES) VALUES (%s, %s, %s, %s, %s, %s)"
+        args = (str(arcg_user_id), arcg_gallows_word,
+                arcg_gallows_status_word, arcg_description, arcg_chars, str(arcg_mistakes), )
+        self.cursor.execute(stmt, args)
         self.conn.commit()
 
-    def add_row_all_users(self, arau_user_id, arau_wins, arau_losses, arau_images_flg):
+    def add_row_all_users(self, arau_user_id, arau_wins, arau_losses, arau_images_flg, arau_lang_flg):
         stmt = "INSERT INTO ALL_USERS (USER_ID, WINS," \
-               "LOSSES, IMAGES_FLG) VALUES (?, ?, ?, ?)"
-        args = (arau_user_id, arau_wins, arau_losses, arau_images_flg, )
-        self.conn.execute(stmt, args)
+               "LOSSES, IMAGES_FLG, LANG_FLG) VALUES (%s, %s, %s, %s, %s)"
+        args = (str(arau_user_id), str(arau_wins), str(arau_losses), str(arau_images_flg), str(arau_lang_flg))
+        self.cursor.execute(stmt, args)
         self.conn.commit()
 
     def delete_row(self, dr_table_name, dr_user_id):
-        stmt = "DELETE FROM {} WHERE USER_ID = (?)".format(dr_table_name)
-        args = (dr_user_id, )
-        self.conn.execute(stmt, args)
+        stmt = "DELETE FROM {} WHERE USER_ID =  %s".format(dr_table_name)
+        args = (str(dr_user_id), )
+        self.cursor.execute(stmt, args)
         self.conn.commit()
 
     def update_field(self, uf_table_name, uf_field_name, uf_field_value, uf_user_id):
-        stmt = "UPDATE {} SET {} = (?) WHERE USER_ID = {}".format(uf_table_name, uf_field_name, uf_user_id)
-        args = (uf_field_value, )
-        self.conn.execute(stmt, args)
+        if uf_field_name in ('WINS', 'LOSSES', 'IMAGES_FLG', 'LANG_FLG', 'MISTAKES'):
+            stmt = "UPDATE {} SET {} = {} WHERE USER_ID = '{}'".format(uf_table_name, uf_field_name,
+                                                                       uf_field_value, uf_user_id)
+        else:
+            stmt = "UPDATE {} SET {} = '{}' WHERE USER_ID = '{}'".format(uf_table_name, uf_field_name,
+                                                                         uf_field_value, uf_user_id)
+        self.cursor.execute(stmt)
         self.conn.commit()
 
     def get_items(self, gi_table_name, gi_field_name, gi_user_id):
-        stmt = "SELECT {} FROM {} WHERE USER_ID = (?)".format(gi_field_name, gi_table_name)
-        args = (gi_user_id,)
+        stmt = "SELECT {} FROM {} WHERE USER_ID = '{}'".format(gi_field_name, gi_table_name, gi_user_id)
+        self.cursor.execute(stmt)
         try:
-            return [x[0] for x in self.conn.execute(stmt, args)][0]
-        except IndexError:
+            return self.cursor.fetchone()[0]
+        except TypeError:
             return False
 
     def new_player(self, np_user_id):
-        self.add_row_all_users(np_user_id, 0, 0, 1)
+        self.add_row_all_users(np_user_id, 0, 0, 1, 1)
 
     def new_game(self, ng_user_id, ng_gallows_word, ng_description, ng_gallows_status_word):
         self.delete_row('CURRENT_GAME', ng_user_id)
@@ -84,14 +96,21 @@ class GallowsEngine:
     def game_begin(self, gb_user_id):
         gb_gallows_word = 'PH'
         gb_description = 'PH'
-        global lines
+        global lines, lines_eng
+        if not self.get_items('ALL_USERS', 'USER_ID', gb_user_id):
+            self.new_player(gb_user_id)
+        gb_lang = self.get_items('ALL_USERS', 'LANG_FLG', gb_user_id)
         while len(gb_gallows_word) < 5:
             index = random.randint(0, len(lines))
-            gb_gallows_word = lines[index].split('|')[0].strip().upper()
-            gb_description = lines[index].split('|')[5].strip().upper()
+            if gb_lang == 0:
+                gb_gallows_word = lines[index].split('|')[0].strip().upper()
+                gb_description = lines[index].split('|')[5].strip().upper()
+            else:
+                gb_gallows_word = lines_eng[index].split(',')[0].strip().upper()
+                gb_gallows_word = re.sub('"', '', gb_gallows_word)
+                gb_description = lines_eng[index].split(',')[-1].strip().upper()
+                gb_description = re.sub('"', '', gb_description)
         gb_gallows_status_word = '_'*len(gb_gallows_word)
-        if not self.get_items('ALL_USERS', 'WINS', gb_user_id):
-            self.new_player(gb_user_id)
         self.new_game(gb_user_id, gb_gallows_word, gb_description, gb_gallows_status_word)
         return gb_gallows_word, gb_gallows_status_word
 
@@ -123,3 +142,73 @@ class GallowsEngine:
             uc_chars = uc_chars+uc_input_char
         self.update_field('CURRENT_GAME', 'CHARS', uc_chars, uc_user_id)
         return uc_chars
+
+    def text_constants(self, tc_user_id):
+        if not self.get_items('ALL_USERS', 'USER_ID',  tc_user_id) \
+                or not self.get_items('CURRENT_GAME', 'USER_ID',  tc_user_id):
+            lang = 0
+            tc_gallows_status_word = list('PLACEHOLDER')
+            tc_gallows_word = 'PLACEHOLDER'
+            tc_mistakes = 0
+            tc_wins = 0
+            tc_losses = 0
+            tc_chars = list('PLACEHOLDER')
+        else:
+            lang = self.get_items('ALL_USERS', 'LANG_FLG', tc_user_id)
+            tc_gallows_status_word = list(self.get_items('CURRENT_GAME',
+                                                         'GALLOWS_STATUS_WORD', tc_user_id))
+            tc_gallows_word = self.get_items('CURRENT_GAME', 'GALLOWS_WORD', tc_user_id)
+            tc_mistakes = self.get_items('CURRENT_GAME', 'MISTAKES', tc_user_id)
+            tc_chars = list(self.get_items('CURRENT_GAME', 'CHARS', tc_user_id))
+            tc_wins = self.get_items('ALL_USERS', 'WINS', tc_user_id)
+            tc_losses = self.get_items('ALL_USERS', 'LOSSES', tc_user_id)
+        if lang == 0:
+            return{'win_text': "{}. Поздравляю с победой![any key чтобы начать заново]".format(
+                                             tc_gallows_word),
+                   'loss_text': "Попытки кончились!\n "
+                                "Твое слово было {} [any key чтобы начать заново]".format(tc_gallows_word),
+                   'turn_text':  "Твое слово\n {} \n Количество оставшихся попыток: {} \n Опробовано {}"
+                                 .format(' '.join(tc_gallows_status_word),
+                                         7 - tc_mistakes, ' '.join(list(tc_chars))),
+                   'session_error_text': "Сессия оборвалась /start чтобы начать сначала",
+                   'wrong_input': "Принимается только кириллица",
+                   'commands_txt': "Команды: /toggle_img - вкл/выкл картинки \n "
+                                   "/start - новая катка \n /my_stats - статистика" 
+                                   " \n /info - все команды \n /hint - подсказка \n /toggle_lang - переключение языка",
+                   'start_text_1': "Я хочу сыграть с тобой в игру",
+                   'start_text_2': "Твое слово \n{}"
+                                   "\n Количество оставшихся попыток: {}".format(' '.join(tc_gallows_status_word), 7),
+                   'stats_text': "Побед: {} Поражений {}".format(int(tc_wins), int(tc_losses)),
+                   'stats_error': "Хммм, походу либо вы новый игрок, либо данные потеряны навсегда",
+                   'pics_off': "Картинки выключены! Чтобы их включить /toggle_img",
+                   'pics_on': "Картинки включены! Чтобы их выключить /toggle_img",
+                   'input_error_1': "За один раз только одна буква",
+                   'input_error_2': "Эта буква уже была. Давай что-нибудь другое",
+                   'lang': "Теперь все на русском. Игра начнется заново",
+                   'hint_deny': "Слишком рано! Подсказку можно просить только если число ошибок больше трех"
+                   }
+        else:
+            return {'win_text': "{}. Victory![any key to continue]".format(
+                tc_gallows_word),
+                'loss_text': "No attempts left!\n "
+                             "Your word was {} [any key to continue]".format(tc_gallows_word),
+                'turn_text': "Your word is \n {} \n Attempts left: {} \n You've already tried {}"
+                .format(' '.join(tc_gallows_status_word),
+                        7 - tc_mistakes, ' '.join(list(tc_chars))),
+                'session_error_text': "Session abruptly ended /start to start again",
+                'wrong_input': "Only latin alphabet is accepted",
+                'commands_txt': "Commands: /toggle_img\n "
+                                "/start\n /my_stats"
+                                " \n /info \n /hint \n /toggle_lang",
+                'start_text_1': "I want to play a game",
+                'start_text_2': "Your word \n{}"
+                                "\n Attempts left: {}".format(' '.join(tc_gallows_status_word), 7),
+                'stats_text': "Wins: {} Losses {}".format(int(tc_wins), int(tc_losses)),
+                'stats_error': "Hmm it looks like your data is lost forever",
+                'pics_off': "Pictures are switched off! To switch them on /toggle_img",
+                'pics_on': "Pictures are switched on! To switch them off /toggle_img",
+                'input_error_1': "Only one letter at a time",
+                'input_error_2': "You've already tried this. Try something different",
+                'lang': "Everything is in english now. The game will start again",
+                'hint_deny': "Too early! You can ask for the hint only after five  failed attempts "
+            }
--- main.py
+++ main.py
@@ -1,39 +1,34 @@
 import telebot
-from telebot import apihelper
+import re
+import os
+#from telebot import apihelper
 from database import GallowsEngine
 
-apihelper.proxy = {proxy}
-bot = telebot.TeleBot(token)
+#apihelper.proxy = {'https': 'socks5h://149.28.35.94:1080'}
+bot = telebot.TeleBot(os.getenv("TOKEN"))
 
-commands_message = "Команды: /toggle_img - вкл/выкл картинки \n /start - новая катка \n /my_stats - статистика" \
-                 " \n /info - все команды \n /hint - подсказка"
 db = GallowsEngine()
 db.setup()
 
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    if not db.get_items('ALL_USERS', 'WINS', message.from_user.id):
-        bot.reply_to(message, commands_message)
+    if not db.get_items('ALL_USERS', 'USER_ID', message.from_user.id):
+        bot.reply_to(message, db.text_constants(message.from_user.id)['commands_txt'])
     db.game_begin(message.from_user.id)
     s_flag = db.get_items('ALL_USERS', 'IMAGES_FLG', message.from_user.id)
-    bot.reply_to(message, "Я хочу сыграть с тобой в игру")
+    bot.reply_to(message, db.text_constants(message.from_user.id)['start_text_1'])
     if s_flag == 1:
         bot.send_photo(message.from_user.id, photo='https://i.ytimg.com/vi/PkWw1gX6xDk/maxresdefault.jpg')
-    s_gallows_status_word = list(db.get_items('CURRENT_GAME', 'GALLOWS_STATUS_WORD', message.from_user.id))
-    bot.send_message(message.from_user.id,
-                     "Твое слово \n{}"
-                     "\n Количество оставшихся попыток: {}".format(' '.join(s_gallows_status_word), 7))
+    bot.send_message(message.from_user.id,  db.text_constants(message.from_user.id)['start_text_2'])
 
 
 @bot.message_handler(commands=['my_stats'])
 def my_stats(message):
     try:
-        ms_wins = db.get_items('ALL_USERS', 'WINS', message.from_user.id)
-        ms_losses = db.get_items('ALL_USERS', 'LOSSES', message.from_user.id)
-        bot.reply_to(message, "Побед: {} Поражений {}".format(int(ms_wins), int(ms_losses)))
+        bot.reply_to(message, db.text_constants(message.from_user.id)['stats_text'])
     except:
-        bot.reply_to(message, "Хммм, походу либо вы новый игрок, либо данные потеряны навсегда")
+        bot.reply_to(message, db.text_constants(message.from_user.id)['stats_error'])
 
 
 @bot.message_handler(commands=['hint'])
@@ -43,7 +38,7 @@ def hint(message):
     if m_mistakes > 3:
         bot.reply_to(message, m_description)
     else:
-        bot.reply_to(message, "Слишком рано! Подсказку можно просить только если число ошибок больше трех")
+        bot.reply_to(message, db.text_constants(message.from_user.id)['hint_deny'])
 
 
 @ bot.message_handler(commands=['cheat'])
@@ -54,7 +49,7 @@ def cheat(message):
 
 @ bot.message_handler(commands=['info'])
 def info(message):
-    bot.reply_to(message, commands_message)
+    bot.reply_to(message, db.text_constants(message.from_user.id)['commands_txt'])
 
 
 @ bot.message_handler(commands=['toggle_img'])
@@ -62,62 +57,70 @@ def toggle(message):
     s_flag = db.get_items('ALL_USERS', 'IMAGES_FLG', message.from_user.id)
     if s_flag == 1:
         db.update_field('ALL_USERS', 'IMAGES_FLG', 0,  message.from_user.id)
-        bot.reply_to(message, "Картинки выключены! Чтобы их включить /toggle_img")
+        bot.reply_to(message, db.text_constants(message.from_user.id)['pics_off'])
     if s_flag == 0:
         db.update_field('ALL_USERS', 'IMAGES_FLG', 1,  message.from_user.id)
-        bot.reply_to(message, "Картинки включены! Чтобы их выключить /toggle_img")
+        bot.reply_to(message, db.text_constants(message.from_user.id)['pics_on'])
 
 
-@bot.message_handler(regexp="[А-ЯёЁ-]")
+@ bot.message_handler(commands=['toggle_lang'])
+def toggle(message):
+    l_flag = db.get_items('ALL_USERS', 'LANG_FLG', message.from_user.id)
+    if l_flag == 1:
+        db.update_field('ALL_USERS', 'LANG_FLG ', 0,  message.from_user.id)
+        bot.reply_to(message, db.text_constants(message.from_user.id)['lang'])
+        bot.register_next_step_handler(message, send_welcome)
+    if l_flag == 0:
+        db.update_field('ALL_USERS', 'LANG_FLG', 1,  message.from_user.id)
+        bot.reply_to(message, db.text_constants(message.from_user.id)['lang'])
+        bot.register_next_step_handler(message, send_welcome)
+
+
+@bot.message_handler()
 def meat(message):
-    if db.get_items('CURRENT_GAME', 'GALLOWS_WORD', message.from_user.id):
-        m_gallows_word = db.get_items('CURRENT_GAME', 'GALLOWS_WORD', message.from_user.id)
-        m_gallows_status_word = db.get_items('CURRENT_GAME', 'GALLOWS_STATUS_WORD', message.from_user.id)
-        m_mistakes = db.get_items('CURRENT_GAME', 'MISTAKES', message.from_user.id)
-        m_chars = db.get_items('CURRENT_GAME', 'CHARS', message.from_user.id)
-        if 7-m_mistakes > 0 and '_' in m_gallows_status_word:
-            if len(message.text) > 1 and message.text.upper() != m_gallows_word:
-                bot.send_message(message.from_user.id, "За один раз только одна буква")
-            elif message.text.upper() in m_chars:
-                bot.send_message(message.from_user.id, "Эта буква уже была. Давай что-нибудь другое")
-            else:
-                if message.text.upper() == m_gallows_word:
-                    db.player_wins(message.from_user.id)
-                    bot.send_message(message.from_user.id,
-                                     "{}. Поздравляю с победой![any key чтобы начать заново]".format(m_gallows_word))
-                    bot.register_next_step_handler(message, send_welcome)
-                else:
-                    db.gallows_check(message.text, message.from_user.id)
-                    m_gallows_status_word = list(db.get_items('CURRENT_GAME',
-                                                              'GALLOWS_STATUS_WORD', message.from_user.id))
-                    m_gallows_word = db.get_items('CURRENT_GAME', 'GALLOWS_WORD', message.from_user.id)
-                    m_mistakes = db.get_items('CURRENT_GAME', 'MISTAKES', message.from_user.id)
-                    m_flag = db.get_items('ALL_USERS', 'IMAGES_FLG', message.from_user.id)
-                    m_chars = list(db.get_items('CURRENT_GAME', 'CHARS', message.from_user.id))
-                    if m_flag == 1:
-                        bot.send_photo(message.from_user.id,
-                                       photo=open('telegrambot/{}.png'.format(m_mistakes), 'rb'))
-                    bot.reply_to(message, "Твое слово\n {} \n Количество оставшихся попыток: {} \n Опробовано {}"
-                                 .format(' '.join(m_gallows_status_word), 7 - m_mistakes, ' '.join(list(set(m_chars)))))
-                    if 7-m_mistakes == 0:
-                        db.player_loses(message.from_user.id)
-                        bot.send_message(message.from_user.id,
-                                         "Попытки кончились!\n "
-                                         "Твое слово было {} [any key чтобы начать заново]".format(m_gallows_word))
-                        bot.register_next_step_handler(message, send_welcome)
-                    if '_' not in m_gallows_status_word:
-                        db.player_wins(message.from_user.id)
-                        bot.send_message(message.from_user.id,
-                                         "{}. Поздравляю с победой![any key чтобы начать заново]".format(
-                                             m_gallows_word))
-                        bot.register_next_step_handler(message, send_welcome)
+    m_flag = db.get_items('ALL_USERS', 'LANG_FLG', message.from_user.id)
+    if m_flag == 0:
+        regexp = "[а-яА-ЯёЁ-]"
     else:
-        bot.send_message(message.from_user.id, "Сессия оборвалась /start чтобы начать сначала")
-
-
-@bot.message_handler(regexp="[^А-ЯёЁ-]")
-def echo_all(message):
-    bot.reply_to(message, "Принимается только кириллица")
+        regexp = "[a-zA-Z-]"
+    if re.match(regexp, message.text):
+        if db.get_items('CURRENT_GAME', 'GALLOWS_WORD', message.from_user.id):
+            m_gallows_word = db.get_items('CURRENT_GAME', 'GALLOWS_WORD', message.from_user.id)
+            m_gallows_status_word = db.get_items('CURRENT_GAME', 'GALLOWS_STATUS_WORD', message.from_user.id)
+            m_mistakes = db.get_items('CURRENT_GAME', 'MISTAKES', message.from_user.id)
+            m_chars = db.get_items('CURRENT_GAME', 'CHARS', message.from_user.id)
+            if 7-m_mistakes > 0 and '_' in m_gallows_status_word:
+                if len(message.text) > 1 and message.text.upper() != m_gallows_word:
+                    bot.send_message(message.from_user.id, db.text_constants(message.from_user.id)['input_error_1'])
+                elif message.text.upper() in m_chars:
+                    bot.send_message(message.from_user.id, db.text_constants(message.from_user.id)['input_error_2'])
+                else:
+                    if message.text.upper() == m_gallows_word:
+                        bot.send_message(message.from_user.id, db.text_constants(message.from_user.id)['win_text'])
+                        db.player_wins(message.from_user.id)
+                        bot.register_next_step_handler(message, send_welcome)
+                    else:
+                        db.gallows_check(message.text, message.from_user.id)
+                        m_gallows_status_word = list(db.get_items('CURRENT_GAME',
+                                                                  'GALLOWS_STATUS_WORD', message.from_user.id))
+                        m_mistakes = db.get_items('CURRENT_GAME', 'MISTAKES', message.from_user.id)
+                        m_flag = db.get_items('ALL_USERS', 'IMAGES_FLG', message.from_user.id)
+                        if m_flag == 1:
+                            bot.send_photo(message.from_user.id,
+                                           photo=open('C:/Users/Tom/Desktop/telega/{}.png'.format(m_mistakes), 'rb'))
+                        bot.reply_to(message, db.text_constants(message.from_user.id)['turn_text'])
+                        if 7-m_mistakes == 0:
+                            bot.send_message(message.from_user.id, db.text_constants(message.from_user.id)['loss_text'])
+                            db.player_loses(message.from_user.id)
+                            bot.register_next_step_handler(message, send_welcome)
+                        if '_' not in m_gallows_status_word:
+                            bot.send_message(message.from_user.id, db.text_constants(message.from_user.id)['win_text'])
+                            db.player_wins(message.from_user.id)
+                            bot.register_next_step_handler(message, send_welcome)
+        else:
+            bot.send_message(message.from_user.id, db.text_constants(message.from_user.id)['session_error_text'])
+    else:
+        bot.reply_to(message, db.text_constants(message.from_user.id)['wrong_input'])
 
 
 bot.polling(none_stop=True)
